f7a3cc25ba3152418dc40496f2e9a0ac41f5e0c0
Darek Desu <4459421+darekdesu@users.noreply.github.com>
2020-10-17 00:46:00 +0200

    Replace Enumerated States with a State Machine

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hello World!');
+const { Machine } = require("xstate");
+
+const lit = {
+  on: {
+    BREAK: "broken",
+    TOGGLE: "unlit",
+  },
+};
+const unlit = {
+  on: {
+    BREAK: "broken",
+    TOGGLE: "lit",
+  },
+};
+const broken = {
+  type: "final",
+};
+
+const states = { lit, unlit, broken };
+
+const initial = "unlit";
+
+const config = {
+  id: "lightBulb",
+  initial,
+  states,
+  strict: true,
+};
+
+const lightBulbMachine = Machine(config);
