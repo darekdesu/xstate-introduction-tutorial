284c98e1974e9a29b032d52d869b72ecd7ae794e
Darek <4459421+darekdesu@users.noreply.github.com>
2020-10-24 01:29:52 +0200

    Trigger Actions When Entering and Exiting a XState State

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const lit = {
   on: {
     BREAK: {
       target: "broken",
-      actions: ["logBroken"],
     },
     TOGGLE: "unlit",
   },
+  exit: () => console.log("no more light! (exit action)"),
 };
 const unlit = {
   on: {
     BREAK: {
       target: "broken",
-      actions: ["logBroken"],
+      actions: () => console.log("(transition action)"),
     },
     TOGGLE: "lit",
   },
 };
 const broken = {
+  entry: (context, event) =>
+    console.log(`I'm broken! with payload: ${event.payload} (entry action)`),
   type: "final",
 };
 
@@ -32,9 +34,9 @@ const config = {
 };
 
 const actions = {
-  logBroken: (context, event) => {
-    console.log(`I'm broken! with payload: ${event.payload}`);
-  },
+  // logBroken: (context, event) => {
+  // console.log(`I'm broken! with payload: ${event.payload}`);
+  // },
 };
 
 const lightBulbMachine = Machine(config, { actions });
@@ -42,14 +44,12 @@ const lightBulbMachine = Machine(config, { actions });
 const service = interpret(lightBulbMachine).start();
 
 service.onTransition((state) => {
-  console.log("Current value:", state.value);
-
+  // console.log("Current value:", state.value);
   // if (state.changed) {
   //   console.log("I`m changed!");
   // } else {
   //   console.log("I`m not changed!");
   // }
-
   // if (state.matches("lit")) {
   //   console.log("I`m lit now!");
   // }
